df1fd942e2f8e49f5883c48129d81f640e1e34b2
Niku Grönberg <gronbergniku@gmail.com>
2023-02-25 16:44:46 +0200

    style button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,34 @@
-import { Component } from 'solid-js'
+import clsx from 'clsx'
+import { Component, JSX, splitProps } from 'solid-js'
 
-interface Props {
+interface Props extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?(event: MouseEvent): void
   text?: string
 }
 
 export const Button: Component<Props> = (props) => {
-  return <button onClick={(event) => props.onClick?.(event)}>{props.text}</button>
+  const [local, others] = splitProps(props, ['onClick', 'text', 'class'])
+
+  return (
+    <button
+      class={clsx(buttonClasses, local.class)}
+      onClick={(event) => local.onClick?.(event)}
+      {...others}
+    >
+      {local.text}
+    </button>
+  )
 }
+
+const buttonClasses = `
+  border
+  border-2
+  border-ctp-mauve
+  hover:border-ctp-blue
+  rounded
+  text-ctp-mauve
+  hover:text-ctp-blue
+  hover:bg-ctp-blue/10
+  font-bold
+  px-4 py-2
+`
