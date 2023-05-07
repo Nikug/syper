2d72f7f118db55fb20c4d7d93f36bf35a30837c5
Niku Grönberg <gronbergniku@gmail.com>
2023-05-07 11:05:08 +0300

    add better loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { getStoredUserOptions, setUserOptions } from './OptionsManager'
 import { setTypingTest } from './StateManager'
 import { initializeText } from './helpers/stateHelpers'
 import { SyncingIndicator } from './components/SyncingIndicator'
+import { LoadingScreen } from './components/LoadingScreen'
 
 const TestPage = lazy(() => import('./components/TestPage'))
 const ProfilePage = lazy(() => import('./components/ProfilePage'))
@@ -14,19 +15,24 @@ const App: Component = () => {
   const [loading, setLoading] = createSignal(false)
 
   onMount(async () => {
+    // Setup user options from local storage
+    setUserOptions(await getStoredUserOptions())
     setLoading(true)
     await setupAuth()
+
+    // Setup user options from Azure if logged in
     setUserOptions(await getStoredUserOptions())
     setupKeyboard()
     setTypingTest(await initializeText())
     setLoading(false)
   })
+
   onCleanup(() => cleanupKeyboard())
 
   return (
     <div class="w-screen font-sans overflow-x-hidden min-h-screen bg-theme-base text-theme-text">
       <SyncingIndicator />
-      <Show when={!loading()} fallback={<p>Loading...</p>}>
+      <Show when={!loading()} fallback={<LoadingScreen />}>
         <Routes>
           <Route path="/" component={TestPage} />
           <Route path="/profile" component={ProfilePage} />
---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,48 @@
+import { Component, createSignal, For, onCleanup, onMount } from 'solid-js'
+
+const dotCount = 15
+const updateInterval = 50
+const sizeMultiplier = 1.5
+
+const getDots = (start: number): number[] => {
+  const step = Math.PI / dotCount
+  const sizes = Array(dotCount)
+    .fill(0)
+    .map((_, index) => ((start + index) % dotCount) * step)
+  const waves = sizes.map((value) =>
+    Math.floor(Math.abs(Math.sin(value) * dotCount * sizeMultiplier))
+  )
+  return waves
+}
+
+export const LoadingScreen: Component = () => {
+  const [dotIndex, setDotIndex] = createSignal<number>(0)
+  const [loadingDots, setLoadingDots] = createSignal<number[]>(getDots(0))
+  const [intervalId, setIntervalId] = createSignal<NodeJS.Timer | undefined>(undefined)
+
+  onMount(() => {
+    const id = setInterval(() => {
+      setDotIndex((dotIndex() + 1) % dotCount)
+      setLoadingDots(getDots(dotIndex()))
+    }, updateInterval)
+    setIntervalId(id)
+  })
+
+  onCleanup(() => clearInterval(intervalId()))
+
+  return (
+    <div class="bg-theme-base w-screen h-screen">
+      <div class="flex flex-col justify-center items-center h-full">
+        <p class="text-3xl font-bold mb-2">Waking up Azure services</p>
+        <p class="mb-8">This usually takes around 15 seconds</p>
+        <div>
+          <For each={loadingDots()}>
+            {(styles) => (
+              <div style={{ height: `${styles}px` }} class="i-ri-cloud-fill inline-block w-8" />
+            )}
+          </For>
+        </div>
+      </div>
+    </div>
+  )
+}
