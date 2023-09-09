6e832edea5a9c07e1e138575357f2340f703c745
Niku Grönberg <gronbergniku@gmail.com>
2023-09-09 11:45:46 +0300

    add supabase auth logic

---
+++ src/authentication/Supabase.ts
@@ -0,0 +1,32 @@
+import { Session, createClient } from '@supabase/supabase-js'
+import { SupabaseConstants } from './constants'
+import { createSignal } from 'solid-js'
+
+export const supabase = createClient(SupabaseConstants.url, SupabaseConstants.anonymousKey)
+
+const [session, setSession] = createSignal<Session | null>(null)
+
+export const setupAuth = async () => {
+  const { data, error } = await supabase.auth.getSession()
+  if (error) return
+  setSession(data.session)
+}
+
+export const isSignedIn = (): boolean => session() != null
+export const getUserName = (): string | null => session()?.user.email ?? null
+export const getUserId = (): string | null => session()?.user.id ?? null
+
+export const signIn = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password })
+  if (error) return
+  if (!data.session && data.user) {
+    console.log('Email not verified')
+  }
+
+  setSession(data.session)
+}
+
+export const signOut = async () => {
+  await supabase.auth.signOut()
+  setSession(null)
+}
