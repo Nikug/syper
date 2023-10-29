e046e443cf98b375b57da9794321a78192de12ab
Niku Grönberg <gronbergniku@gmail.com>
2023-10-29 09:52:37 +0200

    add working progress counter for nontimed tests

--- src/components/QuoteInformation.tsx
+++ src/components/QuoteInformation.tsx
@@ -1,22 +1,31 @@
 import { Component, Show } from 'solid-js'
 import { getShortFormattedDuration } from '../helpers/mathHelpers'
 import { isTimeMode } from '../helpers/optionsHelpers'
-import { testStarted } from '../helpers/stateHelpers'
 import { attempt, typingTest, userOptions } from '../StateManager'
+import { testStarted } from '../helpers/stateHelpers'
 
 export const QuoteInformation: Component = () => {
+  const completedWords = () => attempt.finalText.split(' ').length - 1
+  const totalWords = () => typingTest().words.length
+
   return (
     <div class="truncate">
       <span class="mr-8">{typingTest().source}</span>
-      <Show when={!isTimeMode()}>
-        <span>{typingTest().length} characters</span>
-      </Show>
       <Show when={userOptions.showProgressCounter}>
+        <Show when={!isTimeMode()}>
+          <span>
+            {completedWords()}
+            <span class="mx-1">/</span>
+            {totalWords()}
+          </span>
+        </Show>
         <Show when={isTimeMode()}>
           <span>
             {getShortFormattedDuration(
-              testStarted() ? attempt.remainingDuration : userOptions.timeDuration
+              testStarted() ? userOptions.timeDuration - attempt.remainingDuration : 0
             )}
+            <span class="mx-1">/</span>
+            {getShortFormattedDuration(userOptions.timeDuration)}
           </span>
         </Show>
       </Show>
