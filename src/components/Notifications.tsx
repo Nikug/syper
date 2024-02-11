6141856b4d4687aabdd5e4a6140716cd83a10656
Niku Grönberg <gronbergniku@gmail.com>
2024-02-11 18:35:00 +0200

    remove create notification button

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,21 +1,13 @@
 import { Component, For } from 'solid-js'
-import { addNotification, notifications } from '../NotificationsHandler'
+import { notifications } from '../NotificationsHandler'
 import { Notification } from './Notification'
 
 export const Notifications: Component = () => {
-  const createNotification = () => {
-    addNotification({
-      type: 'error',
-      content: 'This is a notification',
-    })
-  }
-
   return (
     <div class="fixed top-24 right-8 w-72 flex flex-col justify-start items-start gap-4">
       <For each={notifications.notifications}>
         {(notification) => <Notification notification={notification} />}
       </For>
-      <button onClick={() => createNotification()}>Add</button>
     </div>
   )
 }
