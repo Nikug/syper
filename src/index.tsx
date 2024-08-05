11ef11e6f39c869c4c45c2bddcc465b25db16af8
Niku Grönberg <gronbergniku@gmail.com>
2024-08-05 11:10:14 +0300

    remove hash from router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
-import { Router, hashIntegration } from '@solidjs/router'
+import { Router } from '@solidjs/router'
 import 'virtual:uno.css'
 import '@unocss/reset/tailwind.css'
 import './index.css'
@@ -11,7 +11,7 @@ import App from './App'
 render(
   () => (
     <MetaProvider>
-      <Router source={hashIntegration()}>
+      <Router>
         <App />
       </Router>
     </MetaProvider>
